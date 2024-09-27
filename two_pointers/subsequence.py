2235200edfdc74dd18c97e2bd15cf6e8d64f8b43
gabrieldiasmp <gabrieldiasmp@gmail.com>
2024-09-27 12:24:18 +0100

    improving subsequence code

--- two_pointers/subsequence.py
+++ two_pointers/subsequence.py
@@ -1,13 +1,11 @@
 def isSubsequence(s, t):
     sp = tp = 0
 
-    while sp <= len(s) -1 and tp <= len(t) - 1:
+    while sp < len(s) and tp < len(t):
         print(f"lower: {sp} | upper: {tp}")
         if s[sp] == t[tp]:
             sp += 1
-            tp += 1
-        else:
-            tp += 1
+        tp += 1
             
 
     return sp == len(s)
